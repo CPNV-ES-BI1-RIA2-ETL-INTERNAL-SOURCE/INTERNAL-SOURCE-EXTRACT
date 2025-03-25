89ac82055b693cfc210c0c40db2990da0e8ed26a
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-03-25 09:17:34 +0100

    refactor: clean up test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,6 @@ TEST_PDF_PATH = TEST_DATA_DIR / "lausanne_24-12-12.pdf"
 TEST_FIXTURES_PATH = TEST_DATA_DIR / "fixtures"
 
 @pytest.fixture
-def test_pdf_path():
-    return TEST_PDF_PATH
-
-@pytest.fixture
 def expected_json_data():
     with open(TEST_FIXTURES_PATH / "expected_output.json") as f:
         return json.load(f)
